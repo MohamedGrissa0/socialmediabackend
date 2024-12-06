70931a5f44ed80a2fa4cc28f923f03c61c1f7705
MohamedGrissa0 <120946859+MohamedGrissa0@users.noreply.github.com>
2024-12-06 08:42:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use("/api/posts", postRoute);
 // });
 
 // Define the port (from environment variables or default to 5000)
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000 ;
 
 // Function to start the server and connect to MongoDB
 const startServer = async () => {
